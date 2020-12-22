a05a391b3ef57d90804cb18a0d786e62783ee6eb
Eduardo Abreu <abreuo.edu@gmail.com>
2020-12-22 16:33:31 -0300

    todos os testes passam

---
+++ src/shared/infra/typeorm/migrations/1608661470981-AddCustomerIdToOrders.ts
@@ -0,0 +1,33 @@
+import { query } from "express";
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+
+export default class AddCustomerIdToOrders1608661470981 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customer_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomer',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropForeignKey('orders', 'ordersCustomer');
+
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
+
+}
